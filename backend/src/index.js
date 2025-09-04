d82f7a44079cacf46d34e9e75d5441921b507b52
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-04 14:00:53 +0530

    index.js updated for vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -12,7 +11,6 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 // Middleware
 app.use(express.json({ limit: '10mb' }));
@@ -29,18 +27,13 @@ app.use(
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  const frontendPath = path.resolve(__dirname, '../frontend/dist');
-  app.use(express.static(frontendPath));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(frontendPath, 'index.html'));
+// Start server
+if(process.env.NODE_ENV !== 'production'){
+  server.listen(PORT, () => {
+    console.log(`✅ Server is running on port: ${PORT}`);
+    connectDB();
   });
 }
 
-// Start server
-server.listen(PORT, () => {
-  console.log(`✅ Server is running on port: ${PORT}`);
-  connectDB();
-});
+export default server;
