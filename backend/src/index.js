2e0d703098d3d4344e6cc554d0edcc4949ef094d
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-04 14:36:32 +0530

    origin change for vercel

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true,
   })
 );
