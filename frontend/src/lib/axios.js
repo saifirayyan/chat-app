83c8811c626d575945c6aa462f93b450163428d1
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-04 13:47:29 +0530

    vercel.json added

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE ? 'http://localhost:5001/api' : '/api',
+    baseURL: `${import.meta.env.BASE_URL}/api`,
     withCredentials: true
 })
