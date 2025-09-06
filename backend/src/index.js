17bce461017021d5daa797445b1541460fa6d48e
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-06 16:39:25 +0530

    changes completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -11,6 +12,8 @@ import { app, server } from './lib/socket.js';
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 
 // Middleware
 app.use(express.json({ limit: '10mb' }));
@@ -27,16 +30,16 @@ app.use(
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-app.get('/', (req, res) => {
-  res.send('server is live');
-});
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-// Start server
-if(process.env.NODE_ENV !== 'production'){
-  server.listen(PORT, () => {
-    console.log(`✅ Server is running on port: ${PORT}`);
-    connectDB();
-  });
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  })
 }
 
-export default server;
+// Start server
+server.listen(PORT, () => {
+  console.log(`✅ Server is running on port: ${PORT}`);
+  connectDB();
+});
