72271c9c9f78da34caa4bc2a3c0f08d7714cee20
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-07 15:04:46 +0530

    ok

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -32,7 +32,7 @@ const ChatContainer = () => {
   )
 
   return (
-    <div className="flex-1 flex flex-col overflow-auto h-full">
+    <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader />
 
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
   },[selectedUser]);
 
   return (
-    <div className='h-screen bg-base-200'>
+    <div className='h-[100dvh] bg-base-200'>
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-6rem)]">
           <div className="max-sm:relative flex h-full rounded-lg overflow-hidden">
