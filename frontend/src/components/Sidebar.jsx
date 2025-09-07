7137d45528938a0dc455ccb52cb3aa1f455de8db
Rayyan Saifi <saifimohdrayyan@gmail.com>
2025-09-07 14:43:15 +0530

    error solved

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -54,11 +54,11 @@ const Sidebar = ({setShowSidebar, showSidebar}) => {
                 <button
                   key={user._id}
                   onClick={() => {
-                    showSidebar(false);
-                    setSelectedUser(user);
+                      setShowSidebar(false);
+                      setSelectedUser(user);
                   }}
                   className={`
-                    w-full p-3 flex items-center gap-3 hover:bg-base-300 transition-colors
+                    w-full p-3 flex items-center gap-3 cursor-pointer hover:bg-base-300 transition-colors
                     ${selectedUser?._id === user?._id? "bg-base-300 ring-1 ring-base-300" : ""}
                   `}
                 >
